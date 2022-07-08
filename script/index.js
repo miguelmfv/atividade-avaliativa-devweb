65f7997ec06c5049b6332ad75fe957bd864ecc38
Miguiss <100147829+miguelmfv@users.noreply.github.com>
2022-07-07 23:43:00 -0300

    updatenseioq

--- script/index.js
+++ script/index.js
@@ -1,6 +1,19 @@
+var submit = document.querySelector('#enviar')
+
+function mostraralerta() {
+    alert("Reserva feita com sucesso!")
+}
+
+
+
 var chk = document.getElementById('chk')
+var parag = document.getElementsByClassName('.parag')
 
 chk.addEventListener('change', () => {
     document.body.classList.toggle('dark')
-
 })
+
+chk.addEventListener('change', () => {
+    document.getElementById('.parag').classList.toggle('dark') // consegui fazer um paragrafo por pagina ficar com letra branca, com o queryselectorALL ele não troca nada
+})
+
